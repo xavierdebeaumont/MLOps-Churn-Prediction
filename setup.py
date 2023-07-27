1362ed75b1e6ad18bc947dc0d9339914590ae507
xavierdebeaumont <xdebeaumont@outlook.fr>
2023-07-27 10:29:54 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='End to End ML pipeline with MLOps practice implementation',
+    author='xavier_debeaumont',
+    license='MIT',
+)
